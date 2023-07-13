24635e7e6dccbc8283e638b5f75fcf298da7a0cb
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-07-13 20:06:48 +0900

    Rename: 웹 애플리케이션 실행 클래스, artifact 이름 수정
    library 도메인을 담당할 library 패키지와 구분되게 하기 위해 디렉토리 이름변경

--- src/main/java/ksp7331/practice/libraryAPI/LibraryAPIApplication.java
+++ src/main/java/ksp7331/practice/libraryAPI/LibraryAPIApplication.java
@@ -1,13 +1,13 @@
-package ksp7331.practice.library;
+package ksp7331.practice.libraryAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
+		SpringApplication.run(LibraryAPIApplication.class, args);
 	}
 
 }
--- src/test/java/ksp7331/practice/libraryAPI/LibraryAPIApplicationTests.java
+++ src/test/java/ksp7331/practice/libraryAPI/LibraryAPIApplicationTests.java
@@ -1,10 +1,10 @@
-package ksp7331.practice.library;
+package ksp7331.practice.libraryAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LibraryApplicationTests {
+class LibraryAPIApplicationTests {
 
 	@Test
 	void contextLoads() {
